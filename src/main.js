0f7d434d81d68925a1acf6f09c8d2183879f9554
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-10-06 12:33:07 +0200

    faq fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import './js/header';
 import * as howToPlay from './js/howToPlay.js';
-import * as faq from './js/FAQ.js';
+import * as faq from './js/faq.js';
 import * as reviews from './js/reviews.js';
