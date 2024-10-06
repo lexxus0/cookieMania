953934a874791017821700ac36537ebdc3c0946c
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-10-06 12:27:34 +0200

    sections

---
+++ src/js/reviews.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+Swiper.use([Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `
+          <span class="${className}">
+            <svg class="reviewsSvg" width="35" height="10">
+              <use href="../img/icons/sprite.svg#swiper"></use>
+            </svg>
+          </span>`;
+    },
+  },
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 1.5,
+    },
+  },
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 import './js/header';
+import * as howToPlay from './js/howToPlay.js';
+import * as faq from './js/FAQ.js';
+import * as reviews from './js/reviews.js';
